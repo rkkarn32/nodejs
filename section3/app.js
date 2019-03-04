2262cbfb293fc3ce4e36b0aab34a5d5021019020
Ramesh Karan <rk.karn32@gmail.com>
2019-03-04 13:00:36 -0500

    Add  module

--- section3/app.js
+++ section3/app.js
@@ -1,7 +1,9 @@
 var notes = require('./notes')
+const validator = require('validator')
 
 var myFile = notes('notes.txt')
 console.log("File Data: "+myFile)
+console.log(validator.isEmail('ramesh.karn@gmail.com'))
 
 // var name = require('./util')
 // console.log(name)
