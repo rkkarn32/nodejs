6eae13178fffb67181d90d944b057bdb829bb22e
Ramesh Karan <rk.karn32@gmail.com>
2019-03-04 03:42:13 -0500

    adding ignore file, along with small change

--- nodes-app/app.js
+++ nodes-app/app.js
@@ -1,2 +1,3 @@
-const fas = require('fs')
-fas.writeFileSync('notes.txt','This file is written for NodeJs, By Rames')
+const fs = require('fs')
+// fs.writeFileSync('notes.txt','This file is written for NodeJs, By Rames')
+fs.appendFileSync('notes.txt', 'This file is written for NodeJs, By Ramesh \n')
