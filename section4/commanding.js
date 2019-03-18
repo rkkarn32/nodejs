15be76e810a7fc7a65c871c611761fbe196613a0
Ramesh Karan <rk.karn32@gmail.com>
2019-03-17 22:27:13 -0400

    Adding command options for YARGS command

--- section4/commanding.js
+++ section4/commanding.js
@@ -3,38 +3,43 @@ const yargs = require('yargs')
 yargs.version('1.1.1.3')
 // yargs([ '-x', '1', '-y', '2' ]).argv
 // yargs.parse(['-x','y','-x',false])
-console.log(yargs.argv)
+// console.log(yargs.argv)
 
 yargs.command({
-  command: 'substract',
-  describe: 'Substract notes',
-  handler: function(){
-    console.log('This will do substraction')
+  command: 'remove',
+  describe: 'Remove notes',
+  builder: {
+    title : {
+      describe: "Note title",
+      demandOption: true
+    }
+  },
+  handler: function(arg){
+    console.log('This will Remove notes ',arg.title)
   }
 })
 
 yargs.command({
   command:'add',
-  describe: 'To add note',
-  handler: function(){
-    console.log('This will add Note')
+  describe: 'Add note',
+  builder: {
+    title: {
+      title: 'Add title',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: function(args){
+    console.log('This will add a Note with title: ',args.title)
   }
 })
 
-yargs.command('multiple','Used to multiply',function(){
-  console.log('This will multiply')
-}).help()
-.argv
+yargs.command('list','List notes', function(){
+  console.log('This will List all notes')
+})
 
-//
-// yargs
-//   .command('subs', 'make a get HTTP request', {
-//     url: {
-//       alias: 'u',
-//       default: 'http://yargs.js.org/'
-//     }
-//   }, function(){
-//     console.log('Adding new Note')
-//   })
-//   .help()
-//   .argv
+yargs.command('read','Used to read',function(){
+  console.log('This will read statements')
+}).parse()
+
+console.log(yargs.argv)
