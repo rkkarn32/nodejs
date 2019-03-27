33b026726fd496a3420cd3991aea7b4a7c07c021
Ramesh Karan <rk.karn32@gmail.com>
2019-03-26 22:16:50 -0400

    weather app add placee

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,7 +2,7 @@ const request = require('request')
 // const URL = 'https://api.darksky.net/forecast/e0425e3a4a6738b54d98d3adaf303e7b/37.8267,-122.4233'
 const URL = 'https://api.darksky.net/forecast/e0425e3a4a6738b54d98d3adaf303e7b/'
 const TOKEN_MAP ='pk.eyJ1IjoicmtrYXJuMzIiLCJhIjoiY2p0bm42bnV1MGJ4cjQ5bXRubTc4MnVlMyJ9.Ke6ixrT2pxcVU1L7kW_8QA'
-const MAP_URL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20angeles.json?access_token=pk.eyJ1IjoicmtrYXJuMzIiLCJhIjoiY2p0bm42bnV1MGJ4cjQ5bXRubTc4MnVlMyJ9.Ke6ixrT2pxcVU1L7kW_8QA'
+const MAP_URL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/orlando.json?access_token=pk.eyJ1IjoicmtrYXJuMzIiLCJhIjoiY2p0bm42bnV1MGJ4cjQ5bXRubTc4MnVlMyJ9.Ke6ixrT2pxcVU1L7kW_8QA'
 var weatherURL ='';
 	//Getting coardinate for given city name
 	var searchKey = 'Angeles'
@@ -14,6 +14,7 @@ var weatherURL ='';
 		}else{
 			console.log('longitude: '+response.body.features[0].center[0])
 			console.log('lattitude: '+response.body.features[0].center[1])
+			console.log('Place: '+response.body.features[0].place_name)
 			weatherURL = URL+response.body.features[0].center[1]+','+response.body.features[0].center[0]
 			console.log(weatherURL)
 			request({url:weatherURL, json:true}, (error, response,body) => {
