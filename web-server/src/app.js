e2b4c055b34ab111453f39b8a02d6f20eba931ef
Ramesh Karan <rk.karn32@gmail.com>
2019-03-27 09:36:58 -0400

    Adding static files

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,5 +1,10 @@
 const express = require('express')
+const path = require('path')
 const app = express()
+
+const publicDir = path.join(__dirname+'/../public/')
+app.use(express.static(publicDir))
+
 app.get('', (req, res) => {
   res.send('Hello this is from Express')
 })
@@ -7,8 +12,11 @@ app.get('/help',(req, res)=>{
   res.send('I\'m here for help')
 })
 
-app.get('/about',(req, res)=>{
+app.get('/About',(req, res)=>{
   res.send('This is testing our About page')
+})
+app.get('/weather', (req, res)=>{
+  res.send('Nice weather')
 })
 
 app.listen(80, ()=>{
