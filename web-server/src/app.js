1dabb3ca865cf92a0ed849a6fa8ecbae7eae2ba2
Ramesh Karan <rk.karn32@gmail.com>
2019-04-02 23:48:11 -0400

    Weather URL implementation

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,38 +1,75 @@
 // const hbs = require('hbs')
 const express = require('express')
 const path = require('path')
+const hbs = require('hbs')
 const app = express()
 
 const publicDir = path.join(__dirname+'/../public/')
-const viewTemplate = path.join(__dirname+'/../view-template/')
+const viewTemplate = path.join(__dirname+'/../template/view/')
+const partialPath = path.join(__dirname+'/../template/partials/')
 app.set('view engine','hbs')
 app.set('views',viewTemplate)
 app.use(express.static(publicDir))
+hbs.registerPartials(partialPath)
 
 app.get('',(req, res)=>{
   res.render('index',{
     title: 'Weather App',
-    name: 'Ramesh'
+    name: 'Ramesh',
+    message: 'Welcome to my App',
+    createdBy: 'Ramesh'
   })
 })
 
 app.get('/about',(req, res)=>{
   res.render('about',{
     title:'About',
-    message: 'Rending HBS'
+    message: 'Rending HBS',
+    createdBy: 'Ramesh'
   })
 })
 
 app.get('/help',(req, res)=>{
   res.render('Help',{
     title:'Help',
-    message: 'Rending HBS'
+    message: 'Rending HBS',
+    createdBy: 'Ramesh'
   })
 })
 app.get('/weather', (req, res)=>{
-  res.send('Nice weather')
+  if(!req.query.address){
+    return res.send({
+      error: 'Provide address for weather'
+    })
+  }
+  res.send({
+    address:req.query.address
+  })
 })
 
+app.get('/product', (req, res)=>{
+  console.log(req.query)
+  res.send({
+    name:'Name',
+    type: 'Type1'
+  })
+})
+
+app.get('/help/*',(req, res)=>{
+  res.render('error',{
+    title:'Error: Help',
+    message: 'Help '+req.query+' not Found',
+    createdBy: 'Ramesh'
+  })
+})
+
+app.get('*',(req, res)=>{
+  res.render('error',{
+    title:'Error: 404',
+    message: 'Page not found',
+    createdBy: 'Ramesh'
+  })
+})
 app.listen(80, ()=>{
   console.log('Server started, ')
 })
